b7b388e9537fa94adb8789c51915c6f7ce61e0d0
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2021-09-29 10:39:02 -0300

    iniciando

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ import * as config from './modules/swiper__props.js'; //Configs do Swiper
 // import menu from './modules/menu.js'; //js do menu
 
 import Dom from './modules/constructors.js'; //selecionar elementos
-//import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
+import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
 //import comparador from './modules/comparador.js';
 import menus from './modules/menu-bruno.js';
 import menuDropdown from './modules/dropdown.js';
@@ -26,16 +26,25 @@ const telaMenor = window.matchMedia('(max-width: 1024px)').matches;
 
 
 // PAGES 
-const pageHome = new Dom().el("#page__home")
+const pageHome = new Dom().el("#page__home");
+const pageAlinhador = new Dom().el("#page__alinhador");
 
 
 if (pageHome) { // ★ HOME  
     new Dom().bodyClass("body__home");
-    //const swiperProdutos = new Swiper(".swiper-produtos", config.propsProdutos);
-    //const swiperSobre = new Swiper(".swiper__sobre", config.propsProdutos);
+    const swiperProdutos = new Swiper(".swiper-produtos", config.propsProdutos);
+    const swiperParceiros = new Swiper(".swiper-parceiros", config.propsParceiros);
     //menus();
     menuDropdown();
-} 
+}
+
+if (pageAlinhador) { // ★ HOME  
+    new Dom().bodyClass("body__alinhador");
+    const swiperProdutos = new Swiper(".swiper-produtos", config.propsProdutos);
+    const swiperParceiros = new Swiper(".swiper-parceiros", config.propsParceiros);
+    //menus();
+    menuDropdown();
+}
 
 
 document.addEventListener("DOMContentLoaded", () => {
--- assets/js/modules/swiper__props.js
+++ assets/js/modules/swiper__props.js
@@ -33,12 +33,58 @@ const propsProdutos = {
         1280: {
             slidesPerView: 4,
             centeredSlides: false,
-            spaceBetween: 20,
+            spaceBetween: 32,
             
         },
 
     }
 }
+
+
+
+export
+const propsParceiros = {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    // centeredSlides: true,
+
+/*     autoplay: {
+        delay: 1200,
+        disableOnInteraction: true,
+    },
+ */
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1.1,
+            centeredSlides: false,
+            spaceBetween: 16,
+        },
+
+        721: {
+            slidesPerView: 2.2,
+            centeredSlides: false,
+            spaceBetween: 16,
+        },
+        1280: {
+            slidesPerView: 4,
+            centeredSlides: false,
+            spaceBetween: 32,
+            
+        },
+
+    }
+}
+
+
 
 
 
