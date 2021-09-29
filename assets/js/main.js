a2d331a058bb08fcb434f2f9cd4a181fb9049102
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2021-09-29 11:41:35 -0300

    iniciando

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ import * as config from './modules/swiper__props.js'; //Configs do Swiper
 
 import Dom from './modules/constructors.js'; //selecionar elementos
 import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
-//import comparador from './modules/comparador.js';
+import comparador from './modules/comparador.js';
 import menus from './modules/menu-bruno.js';
 import menuDropdown from './modules/dropdown.js';
 //import modalFormulario from './modules/modal-formulario.js';
@@ -44,6 +44,7 @@ if (pageAlinhador) { // ★ HOME
     const swiperParceiros = new Swiper(".swiper-parceiros", config.propsParceiros);
     //menus();
     menuDropdown();
+    comparador();
 }
 
 
